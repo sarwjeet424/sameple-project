5e9a9360835a5fd6dc852754277c153e4327b8b1
Sarwjeet Kumar Srivastava <101391567+sarwjeet424@users.noreply.github.com>
2023-10-13 11:09:51 +0530

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
+  getHello(): object {
     try {
       return process.env;
     } catch (err) {
