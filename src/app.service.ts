519327fdc636d001f60a860d448571675f5b9337
Sarwjeet Kumar Srivastava <101391567+sarwjeet424@users.noreply.github.com>
2023-10-13 09:40:47 +0530

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,10 @@ export class AppService {
   getHello(): string {
     return 'Welcome to the hello world!!';
   }
+    getHealth(): object {
+    return {
+      calories: 50,
+      heartRate: 60,
+    };
+  }
 }
