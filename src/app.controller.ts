e4d1146eb117d74d239fcfcc0307f3dad964fb71
Sarwjeet Kumar Srivastava <101391567+sarwjeet424@users.noreply.github.com>
2023-10-13 09:48:25 +0530

    Update app.controller.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -14,4 +14,9 @@ export class AppController {
   getHealth(): object {
     return this.appService.getHealth();
   }
+
+  @Get('/test')
+  getTest(): object {
+    return this.appService.getTest();
+  }
 }
